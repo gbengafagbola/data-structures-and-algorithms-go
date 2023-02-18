765b9f61000f6a1d35d9b6915c2a98c10b903f3f
holard <fagbolagbenga2015@gmail.com>
2023-02-18 22:30:39 +0100

    max sub array

--- array/4. finding the largest sum subarray/main.go
+++ array/4. finding the largest sum subarray/main.go
@@ -1,28 +1,49 @@
 package main
+
 import "fmt"
 
-// Naive solution of finding maximum sub array sum 
 func MaxSubArraySum(data []int) int {
-    max := data[0]
-    sum := 0
-    end := len(data) - 1
+	sum := 0
+	max := 0
+	length := len(data) - 1
 
-    for i := 0; i <= end; i++ {
-        for j:=i; j <= end; j++ {
-            sum += data[j]
-            if sum > max {
-                max = sum
-            }
-        } 
-        sum = 0
-    }
-    return max
+	for i := 0; i <= length; i++ {
+		sum = sum + data[i]
+
+		if sum < 0 {
+			sum = 0
+		}
+
+		if max < sum {
+			max = sum
+		}
+	}
+	return max
+}
+
+// Naive solution of finding maximum sub array sum
+func NaiveMaxSubArraySum(data []int) int {
+	max := data[0]
+	sum := 0
+	length := len(data) - 1
+
+	for i := 0; i <= length; i++ {
+		for j := i; j <= length; j++ {
+			sum += data[j]
+			if sum > max {
+				max = sum
+			}
+		}
+		sum = 0
+	}
+	return max
 }
 
 // Testing
 func main() {
-	arr := []int{1,-2,3,4,-4,6,-14}
+	arr := []int{1, -2, 3, 4, -4, 6, -14}
 	fmt.Println("MaxSubArraySum:", MaxSubArraySum(arr))
+	// fmt.Println("MaxSubArraySum:", NaiveMaxSubArraySum(arr))
 }
 
 //  Time Complexity
