0d504528c8bdf697bc7fc387644c0f2d8a0768ff
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-01-10 17:20:21 +0100

    bad solution

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import "fmt"
+
+
+
+
+
+func productExceptSelf(nums []int) []int {
+	result := []int{}
+
+	track := 0
+	multiple := 1
+
+	var i int = 0;
+
+	for i < len(nums) && track <= len(nums){
+		if track != i {
+			multiple = multiple * nums[i]
+		}
+		i++
+	}
+	result = append(result, multiple)
+	i = 0;
+	track++
+
+	return result
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	result := productExceptSelf(nums)
+	fmt.Println(result) // Output: [24, 12, 8, 6]
+}
