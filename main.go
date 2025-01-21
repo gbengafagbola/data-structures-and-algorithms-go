731db19854c3a1ef6a88f455afbd6efac571df03
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-01-21 18:15:50 +0100

    formart

--- main.go
+++ main.go
@@ -1,34 +1,27 @@
 package main
 
-import "fmt"
-
-
-
-
-
-func productExceptSelf(nums []int) []int {
-	result := []int{}
-
-	track := 0
-	multiple := 1
-
-	var i int = 0;
-
-	for i < len(nums) && track <= len(nums){
-		if track != i {
-			multiple = multiple * nums[i]
-		}
-		i++
+func isValidSudoku(board [][]byte) []string {	  
+	arr := []string{}
+	for i := 0; i <= len(board); i++ {
+		arr = append(arr, arr[i])
 	}
-	result = append(result, multiple)
-	i = 0;
-	track++
-
-	return result
+    return arr
 }
 
-func main() {
-	nums := []int{1, 2, 3, 4}
-	result := productExceptSelf(nums)
-	fmt.Println(result) // Output: [24, 12, 8, 6]
+
+func main(){
+	board := [][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	}
+
+	result := isValidSudoku(board)
+	println(result)
 }
