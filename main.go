96e9ef170eb58ccc8c9dee5a74ff1ee5fb9b7d7c
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-01-28 13:47:42 +0100

    worker pool

--- main.go
+++ main.go
@@ -1,13 +1,58 @@
+// package main
+
+// import (
+// 	"log"       // Importing log package
+// 	"dsa-golang/generate"
+// )
+
+// func main() {
+// 	// Call the Generate function and handle errors
+// 	if err := generate.Generate(); err != nil {
+// 		log.Fatalf("Error: %v", err)  // Log the error and exit
+// 	}
+// }
+
+
 package main
 
 import (
-	"log"       // Importing log package
-	"dsa-golang/generate"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
-func main() {
-	// Call the Generate function and handle errors
-	if err := generate.Generate(); err != nil {
-		log.Fatalf("Error: %v", err)  // Log the error and exit
+func echoWorker(in, out chan int){
+	for {
+		n := <- in
+
+		time.Sleep(
+			time.Duration(rand.Intn(3000)) * time.Millisecond,
+		)
+
+		out <- n
 	}
+}
+
+func producer(ch chan <- int) {
+	i := 0
+	for {
+		fmt.Printf(" -> send job: %d\n", i)
+		ch <- i
+		i++
+	}
+}
+
+func main() {
+	in := make(chan int)
+	out := make(chan int)
+
+	for i := 0; i < 4; i++ {
+	go echoWorker(in, out)
+}
+	go producer(in)
+
+	for n := range out {
+		fmt.Printf("<- receive job: %d\n", n)
+	}
+
 }
