850db4acec15623c769a884694cf311f7d001e16
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-01-31 15:17:27 +0100

    testing

---
+++ 1.7.testing/test_main.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	a := 5
+	b := 6
+	expected := 11
+	res := sum(a, b)
+	if res != expected {
+		t.Errorf("Our sum function doens't work, %d+%d isn't %d\n", a, b, res)
+	}
+}
+
+
+// to run test 
+// go test -v 
+
+// test coverage
+//go test -cover
