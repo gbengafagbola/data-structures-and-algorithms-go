a76a04a81993ad2c33509edbec2c4113821df4a1
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-02-12 10:57:56 +0100

    update

---
+++ 1.0.array/1.8.smallest-positive-missing-number/missing-number.go
@@ -0,0 +1,35 @@
+package main
+import "fmt"
+
+func SmallestPositiveMissingNumber(arr []int, size int) int {
+	i := 0
+	missing := -1
+
+	for i < size {
+		currentIndex := i + 1
+
+		if arr[i] != currentIndex && arr[i] > size {
+			missing = currentIndex
+			i++
+		}
+
+		if currentIndex < size && arr[i] != currentIndex {
+			arr[i], arr[arr[i]-1] = arr[arr[i]-1], arr[i]
+		}
+
+		if currentIndex < size && arr[i] == i+1 {
+			i++
+		}
+	}
+
+	return missing
+}
+
+func main() {
+	array := []int{1, 3, 4, 2, 6}
+	size := len(array)
+	fmt.Println("array", SmallestPositiveMissingNumber(array, size))
+}
+
+//  Time Complexity
+// O(n)
