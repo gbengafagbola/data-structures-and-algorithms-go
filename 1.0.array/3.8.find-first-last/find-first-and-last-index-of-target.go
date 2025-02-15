60487b7731ef9d862a89ada1ce10d63bfa53503f
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-02-15 22:07:11 +0100

    return the first and last index of target

--- 1.0.array/3.8.find-first-last/find-first-and-last-index-of-target.go
+++ 1.0.array/3.8.find-first-last/find-first-and-last-index-of-target.go
@@ -1,14 +1,17 @@
 package main
-import "fmt"
 
-func target(arr []int, target int) []int {
+import (
+	"fmt"
+	"math"
+)
 
+func targetFirstLast(arr []int, target int) []int {
 	if len(arr) == 0 {
-		return  []int{0, 0}
+		return []int{0, 0}
 	}
 
-	left := 0 
-	right := len(arr)-1
+	left := 0
+	right := len(arr) - 1
 
 	for left <= right {
 
@@ -20,16 +23,62 @@ func target(arr []int, target int) []int {
 			right--
 		}
 
-		if arr[left] == target && arr[right] == target{
+		if arr[left] == target && arr[right] == target {
 			return []int{left, right}
 		}
-
 	}
 	return []int{-1, -1}
+}
+
+func targetFirstLast2(arr []int, target int) []int {
+	first := findFirst(arr, target)
+	last := findLast(arr, target)
+	return []int{first, last}
+}
+
+func findFirst(arr []int, target int) int {
+	left := 0
+	right := len(arr) - 1
+
+	for left <= right {
+		mid := int(math.Floor((float64(left) + float64(right)) / 2))
+		if arr[mid] == target {
+			if mid == 0 || arr[mid-1] != target {
+				return mid
+			} else {
+				right = mid - 1
+			}
+		} else if arr[mid] < target {
+			left = mid + 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
+}
+
+func findLast(arr []int, target int) int {
+	left := 0
+	right := len(arr) - 1
+
+	for left <= right {
+		mid := int(math.Floor((float64(left) + float64(right)) / 2))
+		if arr[mid] == target {
+			if mid == len(arr)-1 || arr[mid+1] != target {
+				return mid
+			}
+			left = mid + 1
+		} else if arr[mid] < target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return -1
 }
 
 func main() {
 	arr := []int{10, 11, 14, 15}
 	tg := 1
-	fmt.Println(target(arr, tg))
+	fmt.Println(targetFirstLast(arr, tg))
 }
