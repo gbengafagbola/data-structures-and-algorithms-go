08b981c9812f4c4f497d99fdf127d7b867b9ab38
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-02-16 23:17:02 +0100

    first bad version

---
+++ 1.0.array/3.9.is-bad-version/bad-version.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func isBadVersion(num int) bool {
+	if num >= 3 {
+		return true
+	}
+	return false
+}
+
+func firstBadVersion(n int) int {
+	left := 1
+	right := n
+	for left < right {
+		mid := left + (right-left)/2 //preventing overflow
+
+		if isBadVersion(mid) {
+			if mid == 1 || (mid-1 > 0 && !isBadVersion(mid-1)) {
+				return mid
+			} else {
+				right = mid
+			}
+		}  else {
+			left = mid + 1
+		}
+
+	}
+	return left
+}
+
+
