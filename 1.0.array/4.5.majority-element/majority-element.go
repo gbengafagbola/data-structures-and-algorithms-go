6bfa30d4d8f171aa2d318a7ae22b1364ea85dec6
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-02-22 19:48:05 +0100

    majority element 2

--- 1.0.array/4.5.majority-element/majority-element.go
+++ 1.0.array/4.5.majority-element/majority-element.go
@@ -17,7 +17,27 @@ func majorityElement(nums []int) int {
 	return 0 
 }
 
+
+
+func majorityElement2(nums []int) int {
+	count := 1
+	majority := nums[0]
+	n:= len(nums)
+	for i := 1; i < n; i++ {
+		if majority == nums[i] {
+			count++
+		} else {
+			count--
+			if count == 0 {
+				majority = nums[i]
+				count = 1
+			}
+		}
+	}
+	return majority
+}
+
 func main () {
-	x := []int{1,3,2,1,1}
-	fmt.Println(majorityElement(x))
+	nums := []int{1,3,2,1,1}
+	fmt.Println(majorityElement(nums))
 }
