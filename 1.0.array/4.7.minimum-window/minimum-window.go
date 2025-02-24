63f6a2b663f9dc6d482917a0f35be9fa4f0c8499
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-02-24 11:58:18 +0100

    optimized minimum window

--- 1.0.array/4.7.minimum-window/minimum-window.go
+++ 1.0.array/4.7.minimum-window/minimum-window.go
@@ -3,9 +3,55 @@ package main
 import "fmt"
 
 
-
-
 func minWindow(s, t string) string {
+	lenString := len(s)
+	lenPattern := len(t)
+
+	if lenString < lenPattern {
+		return ""
+	}
+
+	hashString := [128]int{}
+	hashPattern := [128]int{}
+
+	for i := 0; i < lenPattern; i++ {
+		hashPattern[t[i]]++
+	}
+
+	count, left, startIndex, minLen := 0, 0, -1, int(^uint(0)>>1)
+
+	for right := 0; right < lenString; right++ {
+		c := s[right]
+		hashString[c]++
+
+		if hashPattern[c] > 0 && hashString[c] <= hashPattern[c] {
+			count++
+		}
+
+		for count == lenPattern {
+			windowLength := right - left + 1
+			if minLen > windowLength {
+				minLen = windowLength
+				startIndex = left
+			}
+
+			leftChar := s[left]
+			hashString[leftChar]--
+
+			if hashPattern[leftChar] > 0 && hashString[leftChar] < hashPattern[leftChar] {
+				count--
+			}
+			left++
+		}
+	}
+
+	if startIndex == -1 {
+		return ""
+	}
+	return s[startIndex : startIndex+minLen]
+}
+
+func minWindow2(s, t string) string {
 
 	lenString := len(s)
 	lenPattern:= len(t)
