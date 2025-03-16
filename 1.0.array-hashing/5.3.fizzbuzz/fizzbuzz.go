52028d9b9e7aa5d807fb4d176dfc244bc94cd144
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-03-16 23:58:32 +0100

    fizzbuzz.

--- 1.0.array-hashing/5.3.fizzbuzz/fizzbuzz.go
+++ 1.0.array-hashing/5.3.fizzbuzz/fizzbuzz.go
@@ -2,6 +2,24 @@ package main
 import "fmt"
 
 func fizzBuzz(n int32) {
+    for i := 1; i <= int(n); i++ {
+        divBy3 := (i % 3 == 0)
+        divBy5 := (i % 5 == 0)
+
+        switch {
+        case divBy3 && divBy5:
+            fmt.Println("FizzBuzz")
+        case divBy5:
+            fmt.Println("Buzz")
+        case divBy3:
+            fmt.Println("Fizz")
+        default:
+            fmt.Println(i)
+        }
+    }
+}
+
+func fizzBuzz2(n int32) {
     // Write your code here
     for i := 1; i <= int(n); i++ {     
         switch {
