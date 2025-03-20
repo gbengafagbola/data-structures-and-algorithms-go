3d415093a6d7d3e13ce5b3b0177a6bf853679a91
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-03-20 13:29:52 +0100

    tournament winner

--- 1.0.array-hashing/5.1.tournament-winner/tournament-winner.go
+++ 1.0.array-hashing/5.1.tournament-winner/tournament-winner.go
@@ -3,6 +3,38 @@ package main
 const HOME_TEAM_WON = 1
 
 func TournamentWinner(competitions [][]string, results []int) string {
+    if len(competitions) != len(results) {
+        return ""
+    }
+    
+    maxScore := -1
+    winner := ""
+    
+    m := make(map[string]int)
+    for idx, competition := range competitions {
+        
+        firstTeam := competition[0]
+        secondTeam := competition[1]
+        
+        if results[idx] == 1 {
+            m[firstTeam] += 3
+            if m[firstTeam] > maxScore {
+                maxScore = m[firstTeam]
+                winner = firstTeam
+            }
+        } else if (results[idx] == 0) {
+              m[secondTeam] += 3
+              if m[secondTeam] > maxScore {
+                maxScore = m[secondTeam]
+                winner = secondTeam
+            }
+        }   
+    }
+
+    return winner
+}
+
+func TournamentWinner2(competitions [][]string, results []int) string {
 	// Write your code here.
 
     final := ""
