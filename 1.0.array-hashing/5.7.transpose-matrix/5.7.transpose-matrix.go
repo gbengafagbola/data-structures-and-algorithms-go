4b9dee47db731b0f2d19d4937ec4999594be5476
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-04-10 10:38:28 +0100

    transpose matrix

--- 1.0.array-hashing/5.7.transpose-matrix/5.7.transpose-matrix.go
+++ 1.0.array-hashing/5.7.transpose-matrix/5.7.transpose-matrix.go
@@ -1,11 +1,18 @@
 package main
 
 func TransposeMatrix(matrix [][]int) [][]int {
-for arr := range matrix {
-	
-}
-return [][]int{}
-}
-func main(){
+    if len(matrix) == 0 {
+        return matrix
+    }
 
+    matrixMaxArray := len(matrix[0])
+    result := make([][]int, matrixMaxArray)
+
+    for _, row := range matrix {
+        for i, v := range row {
+            result[i] = append(result[i], v)
+        }
+    }
+
+    return result
 }
