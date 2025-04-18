7856523ca980c1299bff4b2edffb88740982924f
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-04-18 08:14:16 +0100

    to move

--- 1.0.array-hashing/6.0.move-element-to-end/move-element-to-end.go
+++ 1.0.array-hashing/6.0.move-element-to-end/move-element-to-end.go
@@ -1,6 +1,23 @@
 package main
 
 func MoveElementToEnd(array []int, toMove int) []int {
+	left := 0
+	right := len(array) - 1
+
+	for left < right {
+		for left < right && array[right] == toMove {
+			right--
+		}
+		if array[left] == toMove {
+			array[left], array[right] = array[right], array[left]
+		}
+		left++
+	}
+
+	return array
+}
+
+func MoveElementToEnd2(array []int, toMove int) []int {
 	result := []int{}
 	toMoveList := []int{}
 
@@ -11,6 +28,6 @@ func MoveElementToEnd(array []int, toMove int) []int {
 			result = append(result, val)
 		}
 	}
-
+	// spreading individual elements of toMoveList into result, because you can't just merge a slice into another slice so you have to offload one by one
 	return append(result, toMoveList...)
 }
