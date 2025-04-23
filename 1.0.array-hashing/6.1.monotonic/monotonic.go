8d0c96e312f3c57c365018f255fdeb7688b62567
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-04-23 17:52:30 +0100

    monotonic -

--- 1.0.array-hashing/6.1.monotonic/monotonic.go
+++ 1.0.array-hashing/6.1.monotonic/monotonic.go
@@ -1,6 +1,35 @@
 package main
 
+
+
 func IsMonotonic(array []int) bool {
+    if len(array) <= 2 {
+        return true
+    }
+
+    direction := array[1] - array[0]
+
+    for i := 2; i < len(array); i++ {
+        if direction == 0 {
+            direction = array[i] - array[i-1]
+            continue
+        }
+
+        if breaksDirection(direction, array[i-1], array[i]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+func breaksDirection(direction int, prev int, curr int) bool {
+    diff := curr - prev
+    return direction > 0 && diff < 0 || direction < 0 && diff > 0
+}
+
+
+func IsMonotonic2(array []int) bool {
     if len(array) <= 2 {
         return true
     }
@@ -19,7 +48,7 @@ func IsMonotonic(array []int) bool {
         }
     }
 
-    if count < 0 {
+    if count < 0 { 
         count = -count
     }
 
