90876825790a84f956384b30ab71b43cadde1acd
Oluwagbenga Fagbola <fagbolagbenga2015@gmail.com>
2025-05-02 13:25:53 +0100

    spiral traversal recursive

--- 1.0.array-hashing/6.2.spiral-traverse/spiral-traverse.go
+++ 1.0.array-hashing/6.2.spiral-traverse/spiral-traverse.go
@@ -4,47 +4,80 @@ import (
 	"fmt"
 )
 
-func spiralTraverse(array [][]int) []int {
-	result := []int{}
-	if len(array) == 0 || len(array[0]) == 0 {
-		return result
-	}
+func spiralTraverse(matrix [][]int) []int {
+    result := []int{}
+    if len(matrix) == 0 {
+        return result
+    }
 
-	startRow, endRow := 0, len(array)-1
-	startCol, endCol := 0, len(array[0])-1
+    top, bottom := 0, len(matrix)-1
+    left, right := 0, len(matrix[0])-1
 
-	for startRow <= endRow && startCol <= endCol {
-		// Traverse right
-		for col := startCol; col <= endCol; col++ {
-			result = append(result, array[startRow][col])
-		}
+    for top <= bottom && left <= right {
+        for col := left; col <= right; col++ {
+            result = append(result, matrix[top][col])
+        }
+        top++
 
-		// Traverse down
-		for row := startRow + 1; row <= endRow; row++ {
-			result = append(result, array[row][endCol])
-		}
+        for row := top; row <= bottom; row++ {
+            result = append(result, matrix[row][right])
+        }
+        right--
 
-		// Traverse left
-		if startRow < endRow {
-			for col := endCol - 1; col >= startCol; col-- {
-				result = append(result, array[endRow][col])
-			}
-		}
+        if top <= bottom {
+            for col := right; col >= left; col-- {
+                result = append(result, matrix[bottom][col])
+            }
+            bottom--
+        }
 
-		// Traverse up
-		if startCol < endCol {
-			for row := endRow - 1; row > startRow; row-- {
-				result = append(result, array[row][startCol])
-			}
-		}
+        if left <= right {
+            for row := bottom; row >= top; row-- {
+                result = append(result, matrix[row][left])
+            }
+            left++
+        }
+    }
 
-		startRow++
-		endRow--
-		startCol++
-		endCol--
-	}
+    return result
+}
 
-	return result
+
+func spiralTraverseRecursive(matrix [][]int) []int {
+    result := []int{}
+    if len(matrix) == 0 {
+        return result
+    }
+    spiralFill(matrix, 0, len(matrix)-1, 0, len(matrix[0])-1, &result)
+    return result
+}
+
+func spiralFill(matrix [][]int, startRow, endRow, startCol, endCol int, result *[]int) {
+    if startRow > endRow || startCol > endCol {
+        return
+    }
+
+    for col := startCol; col <= endCol; col++ {
+        *result = append(*result, matrix[startRow][col])
+    }
+
+    for row := startRow + 1; row <= endRow; row++ {
+        *result = append(*result, matrix[row][endCol])
+    }
+
+    if startRow < endRow {
+        for col := endCol - 1; col >= startCol; col-- {
+            *result = append(*result, matrix[endRow][col])
+        }
+    }
+
+    if startCol < endCol {
+        for row := endRow - 1; row > startRow; row-- {
+            *result = append(*result, matrix[row][startCol])
+        }
+    }
+
+    spiralFill(matrix, startRow+1, endRow-1, startCol+1, endCol-1, result)
 }
 
 func main() {
