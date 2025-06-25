25c7659fbe25ed7ce8448651791423fb21f6f4e7
gbengafagbola <fagbolagbenga2015@gmail.com>
2025-06-26 00:11:13 +0100

    improved zero index

--- 1.0.array-hashing/3.6.move-zeroes/move-zeroes.go
+++ 1.0.array-hashing/3.6.move-zeroes/move-zeroes.go
@@ -4,6 +4,19 @@ import "fmt"
 
 func moveZeroes(nums []int) []int {
 	zeroIndex := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			if i != zeroIndex {
+				nums[i], nums[zeroIndex] = nums[zeroIndex], nums[i]
+			}
+			zeroIndex++
+		}
+	}
+	return nums
+}
+
+func moveZeroes2(nums []int) []int {
+	zeroIndex := 0
 	n := len(nums)
 
 	for nonZeroIndex := 0; nonZeroIndex < n; nonZeroIndex++ {
