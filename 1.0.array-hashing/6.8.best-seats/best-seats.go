af8801fdb1e925a5dd1822f89b86648a50da6726
gbengafagbola <fagbolagbenga2015@gmail.com>
2025-07-04 11:53:13 +0100

    best seats ipdate

--- 1.0.array-hashing/6.8.best-seats/best-seats.go
+++ 1.0.array-hashing/6.8.best-seats/best-seats.go
@@ -27,6 +27,27 @@ func BestSeat(seats []int) int {
     return result
 }
 
+func BestSeat2(seats []int) int {
+	bestSeat := -1
+	maxSpace := 0
+
+	left := 0 
+	for left < len(seats) {
+		right := left +1 
+		for right < len(seats) && seats[right] == 0 {
+			right += 1
+		}
+		availableSpace := right - left - 1
+		if availableSpace > maxSpace{
+			bestSeat = (left + right) / 2
+			maxSpace = availableSpace
+		}
+		left = right
+	}
+	return bestSeat
+}
+
+
 func main() {
     seats := []int{1, 0, 0, 0, 1, 0, 1}
     fmt.Println("Best seat index:", BestSeat(seats))
