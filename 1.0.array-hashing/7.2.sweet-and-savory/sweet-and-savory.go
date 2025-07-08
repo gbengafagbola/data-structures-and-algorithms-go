b6b11d847821a1b26076111a14fa413a288595b1
gbengafagbola <fagbolagbenga2015@gmail.com>
2025-07-08 22:35:51 +0100

    sweet and savory

---
+++ 1.0.array-hashing/7.2.sweet-and-savory/sweet-and-savory.go
@@ -0,0 +1,26 @@
+package main
+import "sort"
+
+
+func MissingNumbers(nums []int) []int {
+	sort.Ints(nums)
+
+	missing := []int{}
+	expected := 1
+	i := 0
+
+	for len(missing) < 2 {
+		if i < len(nums) && nums[i] == expected {
+			// If current number exists in nums, skip it
+			for i < len(nums) && nums[i] == expected {
+				i++
+			}
+		} else {
+			// If expected is missing
+			missing = append(missing, expected)
+		}
+		expected++
+	}
+
+	return missing
+}
