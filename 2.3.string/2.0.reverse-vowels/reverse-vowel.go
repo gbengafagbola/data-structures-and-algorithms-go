de0bde17221febc0275794fe3ffbb73ff693a00b
gbengafagbola <fagbolagbenga2015@gmail.com>
2025-07-30 23:38:18 +0100

    reverse string constant time  lookup via hashtable implementation

--- 2.3.string/2.0.reverse-vowels/reverse-vowel.go
+++ 2.3.string/2.0.reverse-vowels/reverse-vowel.go
@@ -1,6 +1,38 @@
 package main
-
 func reverseVowels(s string) string {
+    i := 0
+    j := len(s) - 1
+
+    // Convert string to a slice of runes to allow mutation
+    chars := []rune(s)
+
+    vowels := map[rune]bool{
+        'a': true, 'A': true,
+        'e': true, 'E': true,
+        'i': true, 'I': true,
+        'o': true, 'O': true,
+        'u': true, 'U': true,
+    }
+
+    for i < j {
+        if vowels[chars[i]] && vowels[chars[j]] {
+            chars[i], chars[j] = chars[j], chars[i]
+            i++
+            j--
+        } else if !vowels[chars[i]] && vowels[chars[j]] {
+            i++
+        } else if vowels[chars[i]] && !vowels[chars[j]] {
+            j--
+        } else {
+            i++
+            j--
+        }
+    }
+
+    return string(chars)
+}
+
+func reverseVowelsInitial(s string) string {
     i := 0
     j := len(s) - 1
 
@@ -30,3 +62,6 @@ func isVowel(vowel rune) bool {
         vowel == 'i' || vowel == 'I' || vowel == 'o' || vowel == 'O' ||
         vowel == 'u' || vowel == 'U'
 }
+
+
+
