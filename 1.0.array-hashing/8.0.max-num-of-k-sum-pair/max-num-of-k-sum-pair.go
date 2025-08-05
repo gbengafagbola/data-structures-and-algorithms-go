7ac86cd0786759d3d5cca55f01ab3bfcba977c1a
gbengafagbola <fagbolagbenga2015@gmail.com>
2025-08-05 18:56:48 +0100

    max num of k sum pair optimal

--- 1.0.array-hashing/8.0.max-num-of-k-sum-pair/max-num-of-k-sum-pair.go
+++ 1.0.array-hashing/8.0.max-num-of-k-sum-pair/max-num-of-k-sum-pair.go
@@ -1,7 +1,25 @@
 package main
 import "sort"
 
+
 func maxOperations(nums []int, k int) int {
+    count := 0
+    freq := make(map[int]int)
+
+    for _, num := range nums {
+        complement := k - num
+        if freq[complement] > 0 {
+            count++
+            freq[complement]-- // use the complement
+        } else {
+            freq[num]++ // store num for future pairing
+        }
+    }
+
+    return count
+}
+
+func maxOperationsInitial(nums []int, k int) int {
     sort.Ints(nums)
     i, j := 0, len(nums)-1
 
