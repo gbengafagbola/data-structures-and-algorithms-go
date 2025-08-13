5d61304dc3935ce157db8e54df34012efebee830
gbengafagbola <fagbolagbenga2015@gmail.com>
2025-08-13 22:23:04 +0100

    is power of 3

--- 2.5.maths/1.0.power-of-2/power-of-2.go
+++ 2.5.maths/1.0.power-of-2/power-of-2.go
@@ -9,3 +9,18 @@ func isPowerOfTwo(n int) bool {
     }
    return n == 1
 }
+
+// power of 3 
+// same logic apply
+
+func isPowerOfThree(n int) bool {
+    if n == 0 {
+        return false
+    }
+
+    for n%3 == 0 {
+        n /= 3
+    }
+    
+   return n == 1
+}
