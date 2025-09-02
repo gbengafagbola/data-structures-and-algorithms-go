4bf1ab76f7cb69e209749c3aa7a2e2a3c9f76ccf
gbengafagbola <fagbolagbenga2015@gmail.com>
2025-09-02 10:16:32 +0100

    has single cycle implemented via an helper cycle

--- 2.4.graph/1.1.has-single-cycle/has-single-cycle.go
+++ 2.4.graph/1.1.has-single-cycle/has-single-cycle.go
@@ -15,3 +15,32 @@ func hasSingleCycle(array []int) bool {
 	}
 	return currentIdx == 0 
 }
+
+
+
+
+func HasSingleCycleHelperFunc(array []int) bool {
+	nVisitedElement := 0 
+	currentIdx := 0
+	for nVisitedElement <  len(array) {
+		if nVisitedElement > 0 && currentIdx == 0 {
+			return false
+		}
+		nVisitedElement++
+		currentIdx = getNextIdx(currentIdx, array)
+	}
+	return currentIdx == 0 
+}
+
+func getNextIdx(currentIdx int, array []int) int {
+    
+    jump := array[currentIdx]
+    nextIdx := (currentIdx + jump) % len(array)
+    
+    if nextIdx >= 0 {
+        return nextIdx
+    } else {
+      return  nextIdx + len(array)
+    }
+    
+}
